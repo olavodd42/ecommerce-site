e154d1cb01b63e0878e2dfdc074a28e9ac9733d9
olavodalberto <olavodalberto921@gmail.com>
2025-02-03 23:40:42 -0300

    adiciona readme.md

--- backend/controllers/authControllers.test.ts
+++
@@ -1,8 +0,0 @@
-import { register, login, getUser, getAllUsers, updateUser, deleteUser } from './authControllers';
-import { Request, Response } from 'express';
-
-test('should register a user', async () => {
-  const req = {} as Request;
-  const res = {} as Response;
-  // Adicione suas asserções e mocks aqui
-});
